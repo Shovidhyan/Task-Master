f417448b2d5af3457e75e911bd6a9b7e6e4f1565
Shovidhyan <shovidhyan77@gmail.com>
2025-03-06 20:18:01 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Task-Master/',
   optimizeDeps: {
     exclude: ['lucide-react'],
+    
   },
 });
+
+
